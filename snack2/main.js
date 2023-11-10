f53578d6d280f6f5e52f760750cab06ececfd71f
vitodursopettersson <vitodurso98@gmail.com>
2023-11-10 11:25:19 +0100

    fix: else

--- snack2/main.js
+++ snack2/main.js
@@ -1,13 +1,15 @@
 // L’utente inserisce due parole in successione, con due prompt.
-// Il software stampa prima la parola più corta, poi la parola più lunga.
 
 const firstWord = prompt('Inserisci la prima parola');
 const secondWord = prompt('Inserisci la seconda parola');
 
+// Il software stampa prima la parola più corta, poi la parola più lunga.
 if (firstWord.length > secondWord.length) {
-    console.log(firstWord);
+    console.log(firstWord, 'è la parola più lunga');
     console.log(secondWord);
+} else if (secondWord > firstWord) {
+    console.log(secondWord, 'è la parola più lunga');
+    console.log(firstWord);
 } else {
-    console.log(secondWord);
-    console.log(firstWord);
+    console.log(firstWord, secondWord, 'sono egualmente lunghe')
 }
