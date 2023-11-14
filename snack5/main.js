180377c8e2c4b17c588282e233d278d96f5f391c
vitodursopettersson <vitodurso98@gmail.com>
2023-11-14 12:08:10 +0100

    feat: snack5

---
+++ snack5/main.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const n = Number(prompt('Inserisci il numero di Array'));
+const containerArray = []
+
+for (let i = 0; i < n; i++) {
+
+    const arrayItem = []
+
+    for (let i = 0; i < 10; i++) {
+        const randomNumber = Math.floor(Math.random() * 100);
+        arrayItem.push(randomNumber)
+    }
+
+    console.log(arrayItem)
+}
