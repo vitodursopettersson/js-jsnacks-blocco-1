70f3079c6620bb244e370bf6844acfcd5974fcec
vitodursopettersson <vitodurso98@gmail.com>
2023-11-14 17:36:45 +0100

    fix: else if

--- snack2/main.js
+++ snack2/main.js
@@ -6,10 +6,10 @@ const secondWord = prompt('Inserisci la seconda parola');
 // Il software stampa prima la parola più corta, poi la parola più lunga.
 if (firstWord.length > secondWord.length) {
     console.log(firstWord, 'è la parola più lunga');
-    console.log(secondWord);
-} else if (secondWord > firstWord) {
+    console.log(secondWord, 'invece quella più corta');
+} else if (secondWord.length > firstWord.length) {
     console.log(secondWord, 'è la parola più lunga');
-    console.log(firstWord);
+    console.log(firstWord, 'invece quella più corta');
 } else {
     console.log(firstWord, secondWord, 'sono egualmente lunghe')
 }
