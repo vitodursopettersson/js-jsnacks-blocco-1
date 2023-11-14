bb960e8ef6f1f34f7f60a167d6d418d623030e3e
vitodursopettersson <vitodurso98@gmail.com>
2023-11-14 10:03:45 +0100

    feat: snack4 ciclo for

--- snack4/main.js
+++ snack4/main.js
@@ -4,8 +4,8 @@
 const listaNumeri = []
 let sommaNumeri = 0
 
-
-// Fino a
+// Ciclo con while
+/*
 while (sommaNumeri < 50) {
     let itemNumero = Number(prompt('Inserisci un numero'));
     listaNumeri.push(itemNumero);
@@ -15,4 +15,27 @@ while (sommaNumeri < 50) {
     console.log(`La somma è: ${sommaNumeri}`)
 }
 
-console.log(`Hai raggiunto il limite`)
+console.log(`Hai raggiunto il limite`);
+*/
+
+
+// Ciclo con for
+let itemNumero = Number(prompt('Inserisci un numero'));
+listaNumeri.push(itemNumero);
+sommaNumeri += itemNumero;
+
+console.log(`Aggiungo il numero: ${itemNumero}`);
+
+for (let i = 0; i < listaNumeri.length; i++) {
+    if (sommaNumeri < 50) {
+        let itemNumero = Number(prompt('Inserisci un numero'));
+        listaNumeri.push(itemNumero);
+        console.log(`Aggiungo il numero: ${itemNumero}`);
+        sommaNumeri += itemNumero;
+        console.log(`La somma è: ${sommaNumeri}`)
+
+    } else {
+        console.log(`Hai raggiunto il limite`);
+        break
+    }
+}
